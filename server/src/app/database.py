27d00b505fb6548993f89fdbf174e9e03a58b4ac
William Ye <yehaojing@protonmail.com>
2023-02-14 00:27:09 +1100

    replaced sqlite with postgres + pgadmin containers

--- server/src/app/database.py
+++ server/src/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///keyboard.db")
+engine = create_engine("postgresql://user:pw@keeb-database-dev:5432/keeb")
 
 Base = declarative_base()
 
