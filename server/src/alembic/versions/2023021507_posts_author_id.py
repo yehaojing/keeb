a553a6055559370a6a195d90e59399646e154e47
yehaojing <yehaojing@protonmail.com>
2023-02-18 11:53:30 +1100

    schema migrations check for existing columns/fkeys before creation

--- server/src/alembic/versions/2023021507_posts_author_id.py
+++ server/src/alembic/versions/2023021507_posts_author_id.py
@@ -5,9 +5,9 @@ Revises:
 Create Date: 2023-02-15 07:35:46.559138
 
 """
-from alembic import op
+from alembic import op, context
 import sqlalchemy as sa
-
+import logging
 
 # revision identifiers, used by Alembic.
 revision = '2023021507_posts_author_id'
@@ -15,20 +15,41 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+logger = logging.getLogger('schema_migration')
+logger.setLevel(logging.INFO)
+
+migration_context = context.get_context()
+insp = sa.engine.reflection.Inspector.from_engine(migration_context.connection)
+
 
 def upgrade():
-    op.alter_column(
-        'posts',
-        'author_id',
-        type_=sa.Integer,
-        postgresql_using='author_id::int'
-    )
+    if 'author_id' not in [col['name'] for col in insp.get_columns('posts')]:
+        op.alter_column(
+            'posts',
+            'author_id',
+            type_=sa.Integer,
+            postgresql_using='author_id::int'
+        )
 
-    op.create_foreign_key(
-        'posts_author_id_fkey',
-        'posts', 'users',
-        ['author_id'], ['id'],
-    )
+    else:
+        logger.info("'author_id' column already exists in table 'posts'")
+
+    if 'posts_author_id_fkey' not in [
+        fkey['name'] for fkey in insp.get_foreign_keys('posts')
+    ]:
+
+        op.create_foreign_key(
+            'posts_author_id_fkey',
+            'posts', 'users',
+            ['author_id'], ['id'],
+        )
+    else:
+        logger.info(
+            (
+                "'posts_author_id_fkey' foreign key"
+                "already exists in table 'posts'"
+            )
+        )
 
 
 def downgrade():
--- server/src/alembic/versions/2023021523_comments_author_id.py
+++ server/src/alembic/versions/2023021523_comments_author_id.py
@@ -1,13 +1,13 @@
 """add author_id to comments table, make it FKEY to users.id
 
 Revision ID: 2023021523_comments_author_id
-Revises: 2023021507_posts_author_id
+Revises: 2023021507_comments_author_id
 Create Date: 2023-02-15 23:05:10.453672
 
 """
-from alembic import op
+from alembic import op, context
 import sqlalchemy as sa
-
+import logging
 
 # revision identifiers, used by Alembic.
 revision = '2023021523_comments_author_id'
@@ -15,18 +15,42 @@ down_revision = '2023021507_posts_author_id'
 branch_labels = None
 depends_on = None
 
+logger = logging.getLogger('schema_migration')
+logger.setLevel(logging.INFO)
+
+migration_context = context.get_context()
+insp = sa.engine.reflection.Inspector.from_engine(migration_context.connection)
+
 
 def upgrade() -> None:
-    op.add_column(
-        'comments',
-        sa.Column('author_id', sa.Integer)
-    )
+    if 'author_id' not in [
+        col['name'] for col in insp.get_columns('comments')
+    ]:
+        op.alter_column(
+            'comments',
+            'author_id',
+            type_=sa.Integer,
+            postgresql_using='author_id::int'
+        )
+    else:
+        logger.info("'author_id' column already exists in table 'comments'")
 
-    op.create_foreign_key(
-        'comments_author_id_fkey',
-        'comments', 'users',
-        ['author_id'], ['id'],
-    )
+    if 'comments_author_id_fkey' not in [
+        fkey['name'] for fkey in insp.get_foreign_keys('comments')
+    ]:
+
+        op.create_foreign_key(
+            'comments_author_id_fkey',
+            'comments', 'users',
+            ['author_id'], ['id'],
+        )
+    else:
+        logger.info(
+            (
+                "'comments_author_id_fkey' foreign key"
+                "already exists in table 'comments'"
+            )
+        )
 
 
 def downgrade() -> None:
