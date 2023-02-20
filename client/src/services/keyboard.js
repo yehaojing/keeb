0984f65bce76442888dcf38601a17542287bf317
William Ye <yehaojing@protonmail.com>
2023-02-20 23:56:31 +1100

    added new frontend service for /users, changed backend URLs for compose.yaml

--- client/src/services/keyboard.js
+++ client/src/services/keyboard.js
@@ -1,17 +1,18 @@
 import axios from '../utils/apiClient'
+const baseUrl = "/keyboard"
 
 const getAll = () => {
-  const request = axios.get("/");
+  const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
 const postNewKeyboard = (newKeyboard) => {
-  const request = axios.post("/", newKeyboard);
+  const request = axios.post(baseUrl, newKeyboard);
   return request.then((response) => response.data);
 };
 
 const deleteKeyboard = (id) => {
-  const deleteRequest = axios.delete(`/${id}`);
+  const deleteRequest = axios.delete(`${baseUrl}/${id}`);
 
   return deleteRequest.then((response) => response.data);
 };
---
+++ client/src/services/user.js
@@ -0,0 +1,16 @@
+import axios from '../utils/apiClient'
+const baseUrl = "/users/token";
+
+const login = async (username, password) => {
+  const response = await axios.post(baseUrl, new URLSearchParams({
+    username,
+    password
+  }));
+  return response.data;
+};
+
+const exportedObject = {
+  login,
+};
+
+export default exportedObject;
