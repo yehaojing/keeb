0bfe0efd8fa087d69a8b3ec7563b05d8846f9e5d
William Ye <yehaojing@protonmail.com>
2023-03-01 23:05:47 +1100

    mui breadcrumbs integrated with react router

--- client/src/components/Breadcrumbs/index.js
+++ client/src/components/Breadcrumbs/index.js
@@ -1,19 +1,37 @@
 import { Breadcrumbs, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
-const StyledBreadcrumbs = ( { crumbs } ) => {
+const StyledBreadcrumbs = ({ crumbs }) => {
+  const preCrumbs = crumbs.slice(0, -1);
+  const currCrumb = crumbs.slice(-1)[0];
+
   return (
-    crumbs &&
-    <Breadcrumbs>
-      {
-        crumbs.map((crumb) => {
+    crumbs && (
+      <Breadcrumbs>
+        {preCrumbs.map((crumb) => {
           return (
-            <Link key={crumb.link} underline="hover" color="inherit" href={crumb.link}>
+            <Link
+              key={crumb.link}
+              underline="hover"
+              color="inherit"
+              component={RouterLink}
+              to={crumb.link}
+            >
               {crumb.name}
             </Link>
           );
-        })
-      }
-    </Breadcrumbs>
+        })}
+        <Link
+          key={currCrumb.link}
+          underline="hover"
+          color="text.primary"
+          component={RouterLink}
+          to={currCrumb.link}
+        >
+          {currCrumb.name}
+        </Link>
+      </Breadcrumbs>
+    )
   );
 };
 
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -33,8 +33,6 @@ export const PostView = () => {
   };
   useEffect(postHook(id), []);
 
-  console.log(id);
-
   return (
     post && (
       <StyledContainer>
