7d120fd56659faaf74e69959f9c909e15df94bb5
William Ye <yehaojing@protonmail.com>
2023-03-02 08:25:04 +1100

    scrollable autohide navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
-import { Button,Toolbar, Typography } from "@mui/material";
+import { Button, Slide, Toolbar, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 import PropTypes from "prop-types";
 import React from "react";
 import { useCookies } from "react-cookie";
@@ -12,75 +13,83 @@ const NavBar = () => {
   const [login, setLogin, removeLogin] = useCookies(["access_token"]);
   const navigate = useNavigate();
   const classes = useStyles();
+  const trigger = useScrollTrigger();
   return (
-    <AppBar style={classes.header}>
-      <Toolbar
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "row",
-        }}
-      >
-        <Typography variant="h3">Keeb</Typography>
-        <div style={{ alignItems: "flex-end" }}>
-          <Button
-            style={classes.outlineButton}
-            color="inherit"
-            component={Link}
-            to="/"
-          >
-            <Typography variant="h5">Keyboards</Typography>
-          </Button>
-          <Button
-            style={classes.outlineButton}
-            color="inherit"
-            component={Link}
-            to="/social"
-          >
-            <Typography variant="h5">Social</Typography>
-          </Button>
-          <Button
-            color="inherit"
-            style={classes.outlineButton}
-            component={Link}
-            to="/about"
-          >
-            <Typography variant="h5">About</Typography>
-          </Button>
+    <Slide
+      appear={false}
+      direction="down"
+      in={!trigger}
+      addEndListener={() => console.log("This is the end.")}
+    >
+      <AppBar style={classes.header}>
+        <Toolbar
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "row",
+          }}
+        >
+          <Typography variant="h3">Keeb</Typography>
+          <div style={{ alignItems: "flex-end" }}>
+            <Button
+              style={classes.outlineButton}
+              color="inherit"
+              component={Link}
+              to="/"
+            >
+              <Typography variant="h5">Keyboards</Typography>
+            </Button>
+            <Button
+              style={classes.outlineButton}
+              color="inherit"
+              component={Link}
+              to="/social"
+            >
+              <Typography variant="h5">Social</Typography>
+            </Button>
+            <Button
+              color="inherit"
+              style={classes.outlineButton}
+              component={Link}
+              to="/about"
+            >
+              <Typography variant="h5">About</Typography>
+            </Button>
 
-          {login.access_token ? (
-            <>
-              <Button
-                variant="outlined"
-                style={classes.outlineButton}
-                onClick={() => {
-                  removeLogin("access_token");
-                  navigate("/");
-                  window.location.reload();
-                }}
-                color="inherit"
-                component={Link}
-                to="/"
-              >
-                <Typography variant="h5">Logout</Typography>
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button
-                variant="outlined"
-                style={classes.outlineButton}
-                color="inherit"
-                component={Link}
-                to="/login"
-              >
-                <Typography variant="h5">Login</Typography>
-              </Button>
-            </>
-          )}
-        </div>
-      </Toolbar>
-    </AppBar>
+            {login.access_token ? (
+              <>
+                <Button
+                  variant="outlined"
+                  style={classes.outlineButton}
+                  onClick={() => {
+                    removeLogin("access_token");
+                    navigate("/");
+                    window.location.reload();
+                  }}
+                  color="inherit"
+                  component={Link}
+                  to="/"
+                >
+                  <Typography variant="h5">Logout</Typography>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="outlined"
+                  style={classes.outlineButton}
+                  color="inherit"
+                  component={Link}
+                  to="/login"
+                >
+                  <Typography variant="h5">Login</Typography>
+                </Button>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 };
 NavBar.propTypes = {
